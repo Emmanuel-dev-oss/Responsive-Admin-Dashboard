c19f5fb6364946da2bb0346d01aa2e8c6c801182
Emmanuel-dev-oss <emmanuelabidoye@gmail.com>
2025-08-26 09:20:22 -1100

    initial realease of Service Order Admin Dashboard Template

---
+++ script.js
@@ -0,0 +1,21 @@
+// Adding hovered class to selected list item
+let list  = document.querySelectorAll('.navigation li');
+
+list.forEach((item) => item.addEventListener('mouseover', activeLink));
+
+function activeLink() {
+    list.forEach((item) => {
+        item.classList.remove('hovered')
+    });
+    this.classList.add('hovered');
+}
+
+// main toggle
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function () {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+};
